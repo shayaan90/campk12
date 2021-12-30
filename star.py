5e7b20cec3c91b5f7c86c2f681663f058b0c55e6
shayaan90 <96759286+shayaan90@users.noreply.github.com>
2021-12-30 09:58:09 +0530

    Update star.py

--- star.py
+++ star.py
@@ -1,7 +1,7 @@
 def part(n):
-	for i in range(0,n):
-    	for j in range(0,i+1):#nested looping
-        	print("*",end="")    
-		print("\r")#two dimensional array printing,nested loop
+  for i in range(0,n):
+    for j in range(0,i+1):#nested looping
+      print("*",end="")    
+    print("\r")#two dimensional array printing,nested loop
 n=5
 part(n)
